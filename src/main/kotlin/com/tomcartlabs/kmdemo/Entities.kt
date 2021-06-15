c2c39ea6e8ba99dbe87019eca14091f390bbfbc9
Tomcart <rnelson@thetomcart.com>
2021-06-15 10:16:03 -0500

    updated with controller

---
+++ src/main/kotlin/com/tomcartlabs/kmdemo/Entities.kt
@@ -0,0 +1,24 @@
+import com.tomcartlabs.*
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.*
+import com.tomcartlabs.kmdemo.*
+@Entity
+class Article(
+        var title: String,
+        var headline: String,
+        var content: String,
+        @ManyToOne var author: User,
+        var slug: String = title.toSlug(),
+        var addedAt: LocalDateTime = LocalDateTime.now(),
+        @Id @GeneratedValue var id: Long? = null)
+
+
+
+@Entity
+class User(
+        var login: String,
+        var firstname: String,
+        var lastname: String,
+        var description: String? = null,
+        @Id @GeneratedValue var id: Long? = null)
